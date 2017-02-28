b60ec8c3dd4270f907c232634dca349f09d6de8a
Dima Lizun <dimalizun@gmail.com>
2017-02-28 23:13:46 +0300

    add scroll

--- app/js/common.js
+++ app/js/common.js
@@ -69,13 +69,13 @@ $(function() {
 
 
 	// scroll
-  $('a[href^="#"]').click(function () {
-			event.preventDefault();
-			var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-			
-			$('body,html').animate({scrollTop: top}, 1500);
-  });
+	$('a[href^="#"]').click(function () {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
